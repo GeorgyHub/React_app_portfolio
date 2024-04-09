0b0aba27515fd169adf2c67920c5f220750b37ee
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-09 20:55:49 +0300

    first step for app

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
-import Map from './libraries/map.js';
-import Header from './Header.js';
-import Footer from './Footer.js';
+//import Map from './libraries/map.js';
+//import Header from './Header.js';
+//import Footer from './Footer.js';
 
 const App = () => {
   return (
     <div className="App">
-      <Header />
-      <h1>Hello, world!</h1>
-      <Map />
-      <Footer />
+      <header className="Header">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGa-AB4KLa5Q4jYOas-CixGrdSxaXPgDa9wQ2jtQwDAQ&s"/>
+      </header>
+      <nav className='nav'>
+        <div>
+          <a href="">Profile</a>
+        </div>
+        <div>
+         <a href="">Messages</a>
+        </div>
+        <div>
+          <a href="">News</a>
+        </div>
+      </nav>
+      <div className='Content'>
+        <img src="https://static.vecteezy.com/system/resources/thumbnails/012/979/480/small/black-and-blue-abstract-banner-with-polygonal-pattern-template-for-your-header-and-presentation-modern-background-vector.jpg"></img>
+      </div>
     </div>
   );
 }
