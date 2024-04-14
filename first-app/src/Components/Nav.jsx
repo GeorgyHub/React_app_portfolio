89b30f8008543c4a412feb13decf316df2423eb1
Georgy <83170949+GeorgyHub@users.noreply.github.com>
2024-04-14 12:43:06 +0300

    CSS-modules

--- first-app/src/Components/Nav.jsx
+++ first-app/src/Components/Nav.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Nav.css';
 
 const Nav = () => {
     return(
       <nav className='nav'>
-        <div>
+        <div className='Item'>
           <a href="">Profile</a>
         </div>
-        <div>
+        <div className='Item'>
          <a href="">Messages</a>
         </div>
-        <div>
+        <div className='Item'>
           <a href="">News</a>
         </div>
       </nav>
--- first-app/src/Components/Profile.jsx
+++ first-app/src/Components/Profile.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import classes from './Profile.module.css';
+
+// let classes ={
+//   'Content': 'Profile_Content__tD0i1',
+//   'Posts': 'Profile_Posts__d6t3x',
+//   'item': 'Profile_item__\+9rQN',
+// }
 
 const Profile = () => {
     return(
-      <div className='Content'>
+      <div className='{classes.Content}'>
         <div>
          <img src="https://static.vecteezy.com/system/resources/thumbnails/012/979/480/small/black-and-blue-abstract-banner-with-polygonal-pattern-template-for-your-header-and-presentation-modern-background-vector.jpg"></img>
         </div>
@@ -14,17 +21,19 @@ const Profile = () => {
           <div>
             New post
           </div>
-          <div>
-            Post 1
-          </div>
-          <div>
-            Post 2
-          </div>
-          <div>
-            Post 3
-          </div>
-          <div>
-            Post 4
+          <div className='{classes.Posts}'>
+           <div className='{classes.item}'>
+             Post 1
+           </div>
+           <div className='{classes.item}'>
+             Post 2
+           </div>
+           <div className='{classes.item}'>
+             Post 3
+           </div>
+           <div className='{classes.item}'>
+             Post 4
+           </div>
           </div>
         </div>
       </div>
